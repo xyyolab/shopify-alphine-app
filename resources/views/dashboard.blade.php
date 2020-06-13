bca6caceb29ccadd5f929b1d05ae1cb06cd99ed3
XING YAHAO <keidarcy.1015@gmail.com>
2020-06-13 12:06:49 +0900

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
 <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
     <!-- This is an example component -->
     <div id="wrapper" class="container px-4 py-4 mx-auto">
-        @include('partials.activate-modal')
+        {{-- @include('partials.activate-modal') --}}
         <div class="sm:grid sm:h-32 sm:grid-flow-row sm:gap-4 sm:grid-cols-3">
             <x-status type="positive" title="Today's wishists" number="32" growth="9" />
             <x-status type="negative" title="Yesterday's wislists" number="20" growth="20" />
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth.shopify'])->name('home');
 
-Route::view('/products', 'products');
-Route::view('/customers', 'customers');
-Route::view('/settings', 'settings');
+Route::middleware(['auth.shopify'])->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('home');
+
+    Route::view('/products', 'products');
+    Route::view('/customers', 'customers');
+    Route::view('/settings', 'settings');
+    Route::get('/test', function () {
+        $shop = Auth::user();
+        $themes = $shop->api()->rest('GET', '/admin/themes.json')['body']->container['themes'];
+
+        $activeThemeId = '';
+        foreach ($themes as $theme) {
+            if ($theme['role'] === 'main') {
+                $activeThemeId = $theme['id'];
+            }
+        }
+        $snippet = 'Your snippet code';
+
+        $payload = ['asset' => ['key' => 'snippets/wishlikist-app.liquid', 'value' => $snippet]];
+
+        $shop->api()->rest('PUT', 'admin/themes/'.$activeThemeId. '/assets.json', $payload);
+
+        return 'SUCCESS';
+    });
+});
